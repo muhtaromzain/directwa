ee9941d144f87a34d1239fa7c4e0a5b73a4f002d
Muhtarom Zain <muhtaromzain@gmail.com>
2021-06-07 21:22:45 +0800

    DirectWA v.1.0

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// document.getElementById("input-number").value = getSavedValue("input-number");
+// //Save the value function - save it to localStorage as (ID, VALUE)
+// function saveValue(e) {
+//   var id = e.id; // get the sender's id to save it .
+//   var val = e.value; // get the value.
+//   localStorage.setItem(id, val); // Every time user writing something, the localStorage's value will override .
+// }
+
+// //get the saved value function - return the value of "v" from localStorage.
+// function getSavedValue(v) {
+//   if (!localStorage.getItem(v)) {
+//     return ""; // You can change this to your defualt value.
+//   }
+//   return localStorage.getItem(v);
+// }
+
+function Chat() {
+  let number = document.getElementById("input-number").value;
+  let fixNumber = number.substring(1);
+  document
+    .getElementById("btn-chat")
+    .setAttribute("href", `https://wa.me/62${fixNumber}`);
+}
